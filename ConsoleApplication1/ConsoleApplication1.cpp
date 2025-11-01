7855fd09014180521c9cf93b685e41c628fe5d8e
Olly <162823107+DiddyChicken@users.noreply.github.com>
2025-11-01 19:00:36 +0000

    e

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,10 @@ unsigned char bytesPublicKey[crypto_box_PUBLICKEYBYTES];
 string hexPrivateKey;
 string hexPublicKey;
 unsigned char signature[crypto_sign_BYTES];
-string in;
+string command;
+vector<TxInput> TransactionInputs;
+vector<TxOutput> TransactionOutputs;
+vector<UTXO> UTXOs;
 
 void bytesFromHex(unsigned char* out, string hex) {
 	for (size_t i = 0; i < hex.size(); i = i + 2) {
@@ -55,27 +58,49 @@ void hexFromBytes(string &out, const unsigned char* bytes, size_t size) {
 	}
 }
 
-struct TxInput {
+struct UTXO {
+	unsigned char txHash[32];
+	uint64_t outputIndex;
+	uint64_t amount;
+	unsigned char publicKey[32];
 };
+
+struct TxInput {
+	unsigned char prevTxHash[32];
+	uint64_t outputIndex;
+	unsigned char signature[32];
+};
+
 struct TxOutput {
+	uint64_t amount;
+	unsigned char publicKey[32];
 };
 
 struct Transaction {
-	string senderPublicKey;
-	string receiverPublicKey;
-	uint64_t amount;
+	Transaction(uint64_t amountP) {
+		uint64_t amount = amountP;
+		timestamp = time(0);
+	}
+	unsigned char txid[32];
+	vector<TxInput> inputs;
+	vector<TxOutput> outputs;
+	time_t timestamp;
+};
 
-};
-struct UTXO {
-	string ownerPublicKey;
-	uint64_t amount;
-};
 struct BlockHeader {
-
+	unsigned char previousBlockHash[32];
+	unsigned char merkleRoot[32];
+	long timestamp;
+	int nonce;
+	int difficulty;
 };
+
 struct Block {
-
+	BlockHeader header;
+	vector<Transaction> transactions;
+	unsigned char hash[32];
 };
+
 struct Blockchain {
 	vector<Block> chain;
 };
@@ -83,19 +108,21 @@ struct Blockchain {
 	int main()
 	{
 		cout << "Enter your private key or type new to generate one: ";
-		cin >> in;
-		if (in == "new") {
+		cin >> command;
+		if (command == "new") {
 			crypto_box_keypair(bytesPublicKey, bytesPrivateKey);
 			hexFromBytes(hexPublicKey, bytesPublicKey, crypto_box_PUBLICKEYBYTES);
 			hexFromBytes(hexPrivateKey, bytesPrivateKey, crypto_box_SECRETKEYBYTES);
 		}
 		else {
-			bytesFromHex(bytesPrivateKey, in);
+			bytesFromHex(bytesPrivateKey, command);
 			crypto_scalarmult_base(bytesPublicKey, bytesPrivateKey);
 			hexFromBytes(hexPublicKey, bytesPublicKey, crypto_box_PUBLICKEYBYTES);
 			hexFromBytes(hexPrivateKey, bytesPrivateKey, crypto_box_SECRETKEYBYTES);
 		}
 		cout << "Public Key: " << hexPublicKey << endl << "Private Key: " << hexPrivateKey << endl;
+
+		Transaction tx(5);
 
 		crypto_sign_detached(signature, NULL, 'e', 8, bytesPrivateKey);
 		int signVerify = crypto_sign_verify_detached(signature, 'e', 8, bytesPublicKey);
