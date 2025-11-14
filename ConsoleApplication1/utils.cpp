d97e119b006992af8ac5de171eff531d16e53d56
Olly <162823107+DiddyChicken@users.noreply.github.com>
2025-11-14 14:04:38 +0000

    e

--- ConsoleApplication1/utils.cpp
+++ ConsoleApplication1/utils.cpp
@@ -163,7 +163,7 @@ Block formatBlock(const uint8_t* serialisedBlock) {
 	Transaction tx;
 	for (uint32_t txIndex = 0; txIndex < txAmount; txIndex++) {
 		block.transactions.push_back(formatTransaction(serialisedBlock + offset));
-		offset += (sizeof(uint32_t) * 2) + *reinterpret_cast<const uint32_t*>(serialisedBlock + offset) + *reinterpret_cast<const uint32_t*>(serialisedBlock + offset + sizeof(uint32_t));
+		offset += (sizeof(uint32_t)*2) + *reinterpret_cast<const uint32_t*>(serialisedBlock + offset) + *reinterpret_cast<const uint32_t*>(serialisedBlock + offset + sizeof(uint32_t));
 	}
 	return block;
 }
