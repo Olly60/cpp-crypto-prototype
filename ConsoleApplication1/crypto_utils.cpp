b7f976050d29a62016e4f3ddc954c35e0a2a1465
Olly <162823107+DiddyChicken@users.noreply.github.com>
2025-11-16 17:18:38 +0000

    e

--- ConsoleApplication1/crypto_utils.cpp
+++ ConsoleApplication1/crypto_utils.cpp
@@ -78,7 +78,7 @@ template <typename T>
 static std::array<uint8_t, sizeof(T)> putBe(T in) {
     std::array<uint8_t, sizeof(T)> out{};
     for (size_t i = 0; i < sizeof(T); i++) {
-        out[sizeof(T) - 1 - i] = static_cast<uint8_t>(in >> (8 * i));
+        out[(sizeof(T) - 1) - i] = static_cast<uint8_t>(in >> (8 * i));
     }
     return out;
 }
