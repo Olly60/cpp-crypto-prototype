4400dc83607133284aa500c8c3a47ef30fb46cfa
Olly <162823107+DiddyChicken@users.noreply.github.com>
2025-11-26 22:34:11 +0000

    e

--- crypto/network.cpp
+++ crypto/network.cpp
@@ -412,9 +412,6 @@ int main() {
         ioContext.run();
     }
     catch (const std::exception& e) {
-        std::cerr << "Server error: " << e.what() << std::endl;
-        return 1;
+        
     }
-
-    return 0;
 }
